5c871da09e27037e756c6ffb2f575db37b6289d8
Alan Campora <alan.campora@gmail.com>
2018-01-21 20:11:44 -0300

    [ADD] test - expect

--- libs/test.js
+++ libs/test.js
@@ -1,13 +1,32 @@
 const assert = (description, value, expectedValue) => {
     if (value !== expectedValue) {
         console.log(
-            `[Error]: ${description} - Expected to be ${expectedValue} but it was ${value}`
+            `error  : ${description} - Expected to be ${expectedValue} but it was ${value}`
         );
     } else {
-        console.log(`[Sucess]: ${description}`);
+        console.log(`sucess : ${description}`);
     }
 };
 
+const ExpectedValue = (description, value) => {
+    const success = description => console.log(`sucess : ${description}`);
+
+    const toBe = otherValue =>
+        value === otherValue ? success(description) : error(description);
+
+    const error = description =>
+        console.log(
+            `error  : ${description} - Expected to be ${expectedValue} but it was ${value}`
+        );
+
+    return {
+        toBe
+    };
+};
+
+const expect = description => value => ExpectedValue(description, value);
+
 module.exports = {
+    expect,
     assert
 };
